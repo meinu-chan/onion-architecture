a76ea673b26a59f65c3127548c65d8b68b59a1b6
Alex <meinu4an@gmail.com>
2023-10-25 20:35:50 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+const path = require('path')
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.eslint.json'),
+    sourceType: 'module',
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'class',
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid'
+      },
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid'
+      }
+    ],
+    '@typescript-eslint/restrict-template-expressions': [
+      'error',
+      {
+        allowAny: true,
+        allowNullish: true
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { args: 'all', argsIgnorePattern: '^_' }
+    ],
+    'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    'no-empty-function': ['error', { allow: ['constructors'] }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'TSEnumDeclaration',
+        message: 'Please use type unions instead of TypeScript enums.'
+      }
+    ]
+  }
+}
